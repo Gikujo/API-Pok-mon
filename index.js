9549345a766815c20ae3b05ce9f92c1c49e8244b
Gikujo <miguel.segura@outlook.fr>
2023-06-20 15:54:59 +0200

    V02 - avec gestion d'exceptions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// ----- PENSER À DÉFINIR L'URL RACINE EN CAS DE CHANGEMENT D'ADRESSE
+
+
+// **********************************************************************************************************
+// ***************************************** AJOUT DES EXTENSIONS *******************************************
+// **********************************************************************************************************
+
 const express = require('express');
 const Joi = require('joi');     // Outil qui sert à tester la validité d'une entrée
 const cors = require('cors');
@@ -8,8 +15,14 @@ const app = express();
 app.use(express.json()); // Ajoute un middleware pour traiter les requêtes au format JSON
 app.use(cors());
 
+
+const url = "cartes/pokemonList.json";
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}..`));
+
+// **********************************************************************************************************
+// ********************************** ENVOI DES FICHIERS SUR LE SERVEUR *************************************
+// **********************************************************************************************************
 
 // Envoyer le fichier pokemonList
 app.get('/cartes', (req, res) => {
@@ -21,12 +34,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Envoyer le fichier CSS
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, 'styles', 'styles.css'));
+});
+
 // Envoyer le script
 app.get('/script', (req, res) => {
     res.sendFile(path.join(__dirname, 'script', 'main.js'));
 });
 
+// **********************************************************************************************************
+// ********************************************** FONCTIONS *************************************************
+// **********************************************************************************************************
+
+function verifierEntree(entree) {
+    const schema = Joi.object({
+        id: Joi.number().required(),
+        nom: Joi.string().min(3).required(),
+        type: Joi.string().min(3).required(),
+        imageSrc: Joi.string().min(10).required()
+    });
+
+
+    let result = (schema.validate(entree));
+
+    if (result.error) {
+        // res.send(`Votre formulaire n'est pas valide.`);
+        return false;
+    }
+}
+
+// **********************************************************************************************************
 // ***************************************** AFFICHER UN POKEMON ********************************************
+// **********************************************************************************************************
 
 app.get('/cartes/:nom', (req, res) => {
 
@@ -38,7 +79,6 @@ app.get('/cartes/:nom', (req, res) => {
         }
 
         try {
-            console.log(data);
             const nomPokemon = req.params.nom;
             const pokemonData = JSON.parse(data);
             const pokemon = pokemonData.cartesPokemon.find(objet => objet.nom == nomPokemon);
@@ -56,30 +96,42 @@ app.get('/cartes/:nom', (req, res) => {
     });
 });
 
+// **********************************************************************************************************
 // ****************************************** AJOUTER UN POKEMON ********************************************
+// **********************************************************************************************************
 
 app.post('/cartes', (req, res) => {
-    const url = "cartes/pokemonList.json";
 
     fs.readFile('cartes/pokemonList.json', 'utf8', (err, data) => {
         if (err) {
-            console.error('Erreur lors de la lecture du fichier JSON :', err);
             res.status(500).send("Une erreur est survenue lors de la lecture de l'objet JSON Pokémon.");
             return;
         }
 
         try {
-            const nom = req.body;
-            const listePokemon = JSON.parse(data);
-            const cartesPokemon = listePokemon.cartesPokemon;
+            let cartesPokemon = JSON.parse(data).cartesPokemon;
+            let idNouveauPokemon;
 
-            const idNouveauPokemon = cartesPokemon[cartesPokemon.length - 1].id + 1;
+            if (cartesPokemon.length == 0) {
+                idNouveauPokemon = 0;
+            } else {
+                idNouveauPokemon = cartesPokemon[cartesPokemon.length - 1].id + 1;
+            }
+
             let newPokemon = {
                 "id": idNouveauPokemon,
                 "nom": req.body.nom,
                 "type": req.body.type,
                 "imageSrc": req.body.image
             };
+
+            let etat = verifierEntree(newPokemon);
+
+            if (etat == false) {
+                res.status(400).send(`Votre formulaire n'est pas valide`);
+                return;
+            }    // Utilisation de Joi pour valider le formulaire
+
             cartesPokemon.push(newPokemon);
 
             const nouvellesDonnees = {
@@ -90,41 +142,53 @@ app.post('/cartes', (req, res) => {
             console.log(nouvellesDonneesJSON);
 
             fs.writeFile(url, nouvellesDonneesJSON, (err) => {
-                if (err) {
-                    console.error(`Une erreur s'est produite lors de l'implémentation du fichier JSON`, err);
-                } else {
-                    console.log(`Le fichier JSON a été modifié avec succès.`);
-                }
-            })
+                let resultat;
 
-            res.send(`Le fichier JSON a été modifié avec succès.`);
+                if (err) {
+                    resultat = `Une erreur s'est produite lors de l'implémentation du fichier JSON`;
+                    console.error(err);
+                    console.log(`errreur`);
+                } else {
+                    resultat = `Le fichier JSON a été modifié avec succès.`;
+                    console.log(`succès`);
+                }
+
+                res.send(resultat);
+            });
+
         } catch (error) {
+            console.log(`Il y a eu un problème lors de l'envoi du fichier JSON.`);
+            res.status(500).send(error);
         }
     });
 });
 
+// **********************************************************************************************************
 // ***************************************** SUPPRIMER UN POKEMON *******************************************
+// **********************************************************************************************************
 
 app.delete('/supprimer/:id', (req, res) => {
     fs.readFile('cartes/pokemonList.json', 'utf8', (err, data) => {
         if (err) {
             console.error('Erreur lors de la lecture du fichier JSON :', err);
-            res.status(500).send("Une erreur est survenue lors de la lecture de l'objet JSON Pokémon.");
+            res.status(500).send("Une erreur est survenue lors de la lecture du fichier JSON Pokémon.");
             return;
         }
 
         try {
             const idRecherche = parseInt(req.params.id);
-            const pokemonData = JSON.parse(data);
-            const cartesPokemon = pokemonData.cartesPokemon;
+            const cartesPokemon = JSON.parse(data).cartesPokemon;
 
             var index = cartesPokemon.findIndex(function (carte) {
                 return carte.id === idRecherche;
             });
 
+            let pokemonEfface;
             if (index !== -1) {
-                cartesPokemon.splice(index, 1);
+                pokemonEfface = cartesPokemon.splice(index, 1);
             }
+
+            console.log(pokemonEfface[0].nom);
 
             const donneesApresSuppression = {
                 "cartesPokemon": cartesPokemon
@@ -133,14 +197,18 @@ app.delete('/supprimer/:id', (req, res) => {
             const nouvellesDonneesJSON = JSON.stringify(donneesApresSuppression, null, 2);
 
             fs.writeFile("cartes/pokemonList.json", nouvellesDonneesJSON, (err) => {
-                if (err) {
-                    console.error(`Une erreur s'est produite lors de l'implémentation du fichier JSON`, err);
-                } else {
-                    console.log(`Le fichier JSON a été modifié avec succès.`);
-                }
-            })
+                let resultat;
 
-            res.send(nouvellesDonneesJSON);
+                if (err) {
+                    console.error(err);
+                    resultat = `Une erreur s'est produite lors de la mofification du fichier JSON`;
+                } else {
+                    resultat = `${pokemonEfface[0].nom} a été effacé avec succès.`;
+                }
+
+                res.send(resultat);
+            });
+
         } catch (error) {
             console.error('Erreur lors de l\'analyse du fichier JSON :', error);
             res.status(500).send("Une erreur est survenue lors du traitement des données des Pokémon.");
@@ -148,7 +216,9 @@ app.delete('/supprimer/:id', (req, res) => {
     });
 });
 
+// **********************************************************************************************************
 // ***************************************** MODIFIER UN POKEMON ********************************************
+// **********************************************************************************************************
 
 app.put('/modifier/:id', (req, res) => {
     fs.readFile('cartes/pokemonList.json', 'utf8', (err, data) => {
@@ -166,7 +236,13 @@ app.put('/modifier/:id', (req, res) => {
                 return carte.id === idRecherche;
             });
 
-            console.log(`index : ${index}`);
+            let etat = verifierEntree(req.body);   // Utilisation de Joi pour valider le formulaire
+
+            if (etat == false) {
+                console.log(`mauvaise saisie`);
+                res.status(400).send(`Votre formulaire n'est pas valide`);
+                return;
+            }
 
             cartesPokemon[index] = req.body;
 
@@ -177,18 +253,24 @@ app.put('/modifier/:id', (req, res) => {
             const nouvellesDonneesJSON = JSON.stringify(donneesApresModification, null, 2);
 
             fs.writeFile("cartes/pokemonList.json", nouvellesDonneesJSON, (err) => {
-                if (err) {
-                    console.error(`Une erreur s'est produite lors de la modification du fichier JSON`, err);
-                } else {
-                    console.log(`Le fichier JSON a été modifié avec succès.`);
-                }
-            })
+                let resultat;
 
-            res.send(nouvellesDonneesJSON);
+                if (err) {
+                    resultat = `Une erreur s'est produite lors de la modification de la base de données`;
+                    console.error(err);
+                } else {
+                    resultat = `La modification de ${req.body.nom} a été effectuée avec succès.`;
+                }
+
+                res.send(resultat);
+            });
+
         } catch (error) {
             console.error('Erreur lors de l\'analyse du fichier JSON :', error);
             res.status(500).send("Une erreur est survenue lors de la modification des données des Pokémon.");
         }
     });
 });
+
+
 
--- script/main.js
+++ script/main.js
@@ -1,31 +1,202 @@
+// ----- PENSER À DÉFINIR L'URL RACINE EN CAS DE CHANGEMENT D'ADRESSE
+
 const url = "http://localhost:3000";
 
-// *************************** AFFICHAGE DES POKEMON *******************************
+// ******************************************************************************************************************
+// ******************************************** AFFICHAGE DES POKEMON ***********************************************
+// ******************************************************************************************************************
 
 const tableau = document.querySelector('tbody');
 let nomCarte = document.querySelector('#nomCarte');
 let formulaireCarte = document.querySelector('#formulaireCarte');
 
-// Requête GET pour afficher tous les Pokemons
+// ----- Requête GET pour afficher tous les Pokemons
 $.ajax({
     url: `${url}/cartes`,
     type: 'GET',
     dataType: 'json',
     success: (data) => {
-        console.log(data);
         const listePokemon = data.cartesPokemon;
-        console.log(listePokemon);
-
         afficher(listePokemon);
     },
     error: () => {
         alert(`Il y a eu un problème lors de l'affichage des Pokémon.`);
     }
-
-
 });
 
-// Fonction pour afficher un array de Pokemons
+// ******************************************************************************************************************
+// ************************************* AFFICHAGE D'UN POKEMON SUR RECHERCHE ***************************************
+// ******************************************************************************************************************
+
+formulaireCarte.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nomPokemon = nomCarte.value;
+
+    if (nomPokemon == '') {
+        alert('Vous devez rentrer un nom de Pokémon.');
+    } else {
+        $.ajax({
+            url: `${url}/cartes/${nomPokemon}`,
+            type: "GET",
+            dataType: "json",
+            success: (data) => {
+                tableau.innerHTML = '';
+                afficher([data]);
+                let boutonAfficher = document.querySelector('#afficher');
+                boutonAfficher.textContent = 'Afficher tous les Pokémons';
+
+                boutonAfficher.addEventListener('click', () => {
+                    location.reload();
+                });
+            },
+            error: () => {
+                alert(`Il y a eu un problème lors de l'affichage de ${nomPokemon}`);
+            }
+        });
+    }
+})
+
+// ******************************************************************************************************************
+// *********************************************** AJOUT DE POKEMON *************************************************
+// ******************************************************************************************************************
+
+const formulaireAjout = document.querySelector('#formulaireAjout');
+const nomAjout = document.querySelector('#nom');
+const typeAjout = document.querySelector('#type');
+const imageAjout = document.querySelector('#imageSrc');
+
+formulaireAjout.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = {
+        "nom": `${nomAjout.value}`,
+        "type": `${typeAjout.value}`,
+        "image": `${imageAjout.value}`
+    };
+
+
+    $.ajax({
+        type: "POST",
+        url: `${url}/cartes`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(formData),
+        dataType: "json",
+        success: (result) => {
+            console.log('oui'); // SUPPRIMER DES RESOLUTION DU PROBLEME
+            alert(result);
+            console.log(result);
+        },
+        error: (xhr, status, error) => {
+            console.log('non'); // SUPPRIMER DES RESOLUTION DU PROBLEME
+            alert(xhr.responseText);
+            location.reload();
+        }
+    });
+});
+
+// ******************************************************************************************************************
+// ******************************************* SUPPRESSION D'UN POKEMON *********************************************
+// ******************************************************************************************************************
+
+
+function ajouterEcouteursSuppression() {
+    const tousFormulairesSuppression = document.querySelectorAll('.formulaireSuppression');
+
+    tousFormulairesSuppression.forEach(formulaire => {
+
+        formulaire.addEventListener('submit', (e) => {
+            e.preventDefault();
+            $.ajax({
+                type: "DELETE",
+                url: formulaire.action,
+                success: function (result) {
+                    alert(result);
+                    location.reload();
+                },
+                error: function (xhr, status, error) {
+                    console.error(`Une erreur s'est produite lors de la requête DELETE:`, error);
+                    alert(result);
+                    location.reload();
+                }
+            });
+        });
+    });
+}
+
+// ******************************************************************************************************************
+// ****************************************** MODIFICATION D'UN POKÉMON *********************************************
+// ******************************************************************************************************************
+
+
+function ajouterEcouteursModification() {
+    const tousBoutonsModification = document.querySelectorAll('.afficherModifications');
+
+    tousBoutonsModification.forEach(bouton => {
+
+        let idAModifier = parseInt(bouton.id.replace('modifier', ''));
+
+        bouton.addEventListener('click', () => {
+
+            const ligneFormulaireModification = document.querySelector(`#ligneFormulaireModification${idAModifier}`);
+            ligneFormulaireModification.classList.remove('d-none');
+
+            const formulaire = document.querySelector(`#formulaireModification${idAModifier}`);
+            console.log(`Vous avez choisi le formulaire vers ${formulaire.action}`);
+
+            formulaire.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                console.log(`Formulaire n°${idAModifier} envoyé`);
+
+                const nouveauNom = document.querySelector(`input[name="newName${idAModifier}"]`).value;
+                const nouveauType = document.querySelector(`input[name="newType${idAModifier}"]`).value;
+                const nouvelleImage = document.querySelector(`input[name="newImage${idAModifier}"]`).value;
+
+                const pokemonModifie = JSON.stringify({
+                    "id": idAModifier,
+                    "nom": nouveauNom,
+                    "type": nouveauType,
+                    "imageSrc": nouvelleImage
+                });
+
+                console.log(pokemonModifie);
+
+                $.ajax({
+                    type: "PUT",
+                    url: formulaire.action,
+                    data: pokemonModifie,
+                    contentType: 'application/json',
+                    success: function (result) {
+                        alert(result);
+                        location.reload();
+                    },
+                    error: function (xhr, status, error) {
+                        alert(error);
+                        location.reload();
+                    }
+                })
+
+            });
+
+            const effacerFormulaire = document.querySelector(`#annuler${idAModifier}`);
+            effacerFormulaire.addEventListener('click', (e) => {
+                e.preventDefault();
+                ligneFormulaireModification.classList.add('d-none');
+                return;
+            });
+        })
+    });
+}
+
+// ******************************************************************************************************************
+// ************************************************** FONCTIONS *****************************************************
+// ******************************************************************************************************************
+
+
+
+// ----- Fonction pour afficher un array de Pokemons
 function afficher(table) {
 
     table.forEach(element => {
@@ -62,159 +233,10 @@ function afficher(table) {
                     </th>
                 </form>
             </tr>`;
-
     });
 
 
     ajouterEcouteursSuppression();
     ajouterEcouteursModification();
 
-}
-
-// Gestion du formulaire de sélection d'un Pokémon
-formulaireCarte.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let nomPokemon = nomCarte.value;
-    console.log(nomPokemon);
-
-    if (nomPokemon == '') {
-        alert('Vous devez rentrer un nom de Pokémon.');
-    } else {
-        $.ajax({
-            url: `${url}/cartes/${nomPokemon}`,
-            type: "GET",
-            dataType: "json",
-            success: (data) => {
-                tableau.innerHTML = '';
-                afficher([data]);
-            },
-            error: () => {
-                alert(`Il y a eu un problème lors de l'affichage de ${nomPokemon}`);
-            }
-        });
-    }
-})
-
-// ****************************** AJOUT DE POKEMON *********************************
-
-const formulaireAjout = document.querySelector('#formulaireAjout');
-const nomAjout = document.querySelector('#nom');
-const typeAjout = document.querySelector('#type');
-const imageAjout = document.querySelector('#imageSrc');
-
-formulaireAjout.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const formData = {
-        "nom": `${nomAjout.value}`,
-        "type": `${typeAjout.value}`,
-        "image": `${imageAjout.value}`
-    };
-    $.ajax({
-        type: "POST",
-        url: `${url}/cartes`,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        data: JSON.stringify(formData),
-        dataType: "json",
-        success: (data) => {
-            // localStorage.setItem(cartesPokemon, data);
-
-            // let listeCartes = localStorage.getItem(cartesPokemon);
-            // console.log(listeCartes);
-        },
-        error: (xhr, status, error) => {
-            console.log(formData);
-
-            // Handle the error response
-        }
-    });
-});
-
-// ************************** SUPPRESSION D'UN POKEMON *****************************
-
-function ajouterEcouteursSuppression() {
-    const tousFormulairesSuppression = document.querySelectorAll('.formulaireSuppression');
-
-    tousFormulairesSuppression.forEach(formulaire => {
-
-        formulaire.addEventListener('submit', (e) => {
-            e.preventDefault();
-            $.ajax({
-                type: "DELETE",
-                url: formulaire.action,
-                success: function (result) {
-                    console.log('La requête DELETE a été effectuée avec succès.', result);
-                },
-                error: function (xhr, status, error) {
-                    console.error('Une erreur s\'est produite lors de la requête DELETE:', error);
-                    // Gérer l'erreur de la requête
-                }
-            });
-        })
-    });
-}
-
-// ********************************* MODIFICATION D'UN POKÉMON ***********************************
-
-function ajouterEcouteursModification() {
-    const tousBoutonsModification = document.querySelectorAll('.afficherModifications');
-
-    tousBoutonsModification.forEach(bouton => {
-
-        const idAModifier = parseInt(bouton.id[bouton.id.length - 1]);
-        console.log(idAModifier);
-
-        bouton.addEventListener('click', () => {
-
-            const ligneFormulaireModification = document.querySelector(`#ligneFormulaireModification${idAModifier}`);
-            ligneFormulaireModification.classList.remove('d-none');
-
-            const formulaire = document.querySelector(`#formulaireModification${idAModifier}`);
-            console.log(`Vous avez choisi le formulaire vers ${formulaire.action}`);
-
-            formulaire.addEventListener('submit', (e) => {
-                e.preventDefault();
-
-                console.log(`Formulaire n°${idAModifier} envoyé`);
-
-                const nouveauNom = document.querySelector(`input[name="newName${idAModifier}"]`).value;
-                const nouveauType = document.querySelector(`input[name="newType${idAModifier}"]`).value;
-                const nouvelleImage = document.querySelector(`input[name="newImage${idAModifier}"]`).value;
-
-                const pokemonModifie = JSON.stringify({
-                    "id": idAModifier,
-                    "nom": nouveauNom,
-                    "type": nouveauType,
-                    "imageSrc": nouvelleImage
-                });
-
-                console.log(pokemonModifie);
-
-                $.ajax({
-                    type: "PUT",
-                    url: formulaire.action,
-                    data: pokemonModifie,
-                    contentType: 'application/json',
-                    success: function (result) {
-                        console.log(result);
-                        // Faire quelque chose avec la réponse en cas de succès
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Une erreur s\'est produite lors de la requête PUT', error);
-                        // Gérer l'erreur de la requête
-                    }
-                })
-
-            });
-
-            const effacerFormulaire = document.querySelector(`#annuler${idAModifier}`);
-            effacerFormulaire.addEventListener('click', (e) => {
-                e.preventDefault();
-                ligneFormulaireModification.classList.add('d-none');
-                return;
-            });
-        })
-    });
 }
